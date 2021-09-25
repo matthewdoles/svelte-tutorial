68d3cb6899799efbcaa85f7547e96119a9592163
Matthew Doles <matteddied@gmail.com>
2021-09-25 17:16:19 -0400

    1.1 - First App & Writing Svelte Code

---
+++ first-app/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let name = 'Matthew';
+  export let age = 27;
+
+  function incrementAge() {
+    age += 1;
+  }
+</script>
+
+<h1>Hello {name}, my age is {age}</h1>
+<button on:click={incrementAge}>Change Age</button>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
