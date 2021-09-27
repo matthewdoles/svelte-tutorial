995a5a9f180f12d69edab18482bf9291cd9dcc5d
Matthew Doles <matteddied@gmail.com>
2021-09-27 19:22:48 -0400

    2.2 - Binding to Element Properties & Two-Way Binding

--- core-features/src/App.svelte
+++ core-features/src/App.svelte
@@ -17,11 +17,18 @@
   function changeName() {
     name = 'Matt';
   }
+
+  function nameInput(e) {
+    name = e.target.value;
+  }
 </script>
 
 <h1>Hello {upperCaseName}, my age is {age}</h1>
 <button on:click={incrementAge}>Change Age</button>
-<button on:click={changeName}>Change Name</button>
+<!-- <button on:click={changeName}>Change Name</button>
+
+<input type="text" value={name} on:input={nameInput} /> -->
+<input type="text" bind:value={name} />
 
 <style>
   h1 {
