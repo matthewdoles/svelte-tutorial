f4e8298f6c48cb29bd08289d21c6e7e71a03e739
Matthew Doles <matteddied@gmail.com>
2021-09-27 19:17:46 -0400

    2.1 - Reactive Variable

---
+++ core-features/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let name = 'Matthew';
+  export let age = 26;
+
+  $: upperCaseName = name.toUpperCase();
+
+  $: console.log(name);
+
+  $: if (name === 'Matt') {
+    age = 27;
+  }
+
+  function incrementAge() {
+    age += 1;
+  }
+
+  function changeName() {
+    name = 'Matt';
+  }
+</script>
+
+<h1>Hello {upperCaseName}, my age is {age}</h1>
+<button on:click={incrementAge}>Change Age</button>
+<button on:click={changeName}>Change Name</button>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
