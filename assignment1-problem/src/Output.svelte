673a896e68e2949b2e1838f7f0b4be3fe73e1097
Matthew Doles <matteddied@gmail.com>
2021-10-04 18:46:45 -0400

    2.6 - Assignment 1: The Basics

---
+++ assignment1-problem/src/Output.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let goal;
+  $: isHighlighted = goal.includes('!');
+</script>
+
+<h1 class:highlight={isHighlighted}>{goal}</h1>
+
+<style>
+  .highlight {
+    color: red;
+  }
+</style>
