75f6765d3ead47ebb10bfefb89b8e1f1af3faaa0
Matthew Doles <matteddied@gmail.com>
2021-10-19 17:56:58 -0400

    6.1 - Event Forwarding & Custom Events

---
+++ components-and-communication/src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Product from './Product.svelte';
+</script>
+
+<Product
+  productTitle="A Book"
+  on:add-to-cart={() => alert('Add to cart!')}
+  on:delete={() => alert('Deleted!')}
+/>
