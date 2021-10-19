77a5eefcddc5c908cc693220c4623b4c2ac1778d
Matthew Doles <matteddied@gmail.com>
2021-10-18 20:57:43 -0400

    5.4 - MeetupGrid Component & Meetups Form

--- course-project/src/App.svelte
+++ course-project/src/App.svelte
@@ -1,15 +1,22 @@
 <script>
   import Header from './UI/Header.svelte';
-  import MeetupItem from './Meetups/MeetupItem.svelte';
+  import MeetupGrid from './Meetups/MeetupGrid.svelte';
 
-  const meetups = [
+  let title = '';
+  let subtitle = '';
+  let address = '';
+  let email = '';
+  let description = '';
+  let imageUrl = '';
+
+  let meetups = [
     {
       id: 'm1',
       title: 'Coding Bootcamp',
       subtitle: 'Learn to code in 2 hours',
       description:
         'In this meetup, we will have some experts that teach you how to code',
-      imageURL:
+      imageUrl:
         'https://www.lighthouselabs.ca/uploads/post/open_graph_image/459/Coding-vs-programming.jpg',
       address: '27th Street Road, 32523 New York',
       contactEmail: 'code@test.com',
@@ -19,30 +26,62 @@
       title: 'Swim Together',
       subtitle: "Let's go swimming",
       description: 'We will simply swim some laps',
-      imageURL:
+      imageUrl:
         'https://www.ymcarichmond.org/sites/default/files/2021-01/serena-repice-lentini-TVOAbbLL050-unsplash.jpg',
       address: '27th Street Road, 32523 New York',
       contactEmail: 'code@test.com',
     },
   ];
+
+  function addMeetup() {
+    const newMeetup = {
+      id: Math.random().toString(),
+      title,
+      subtitle,
+      description,
+      imageUrl,
+      address,
+      contactEmail: email,
+    };
+    meetups = [...meetups, newMeetup];
+  }
 </script>
 
 <Header />
-<section id="meetups">
-  {#each meetups as meetup}
-    <MeetupItem
-      title={meetup.title}
-      subtitle={meetup.subtitle}
-      description={meetup.description}
-      imageUrl={meetup.imageURL}
-      address={meetup.description}
-      email={meetup.contactEmail}
-    />
-  {/each}
-</section>
+
+<main>
+  <form on:submit|preventDefault={addMeetup}>
+    <div class="form-control">
+      <label for="title">Title</label>
+      <input type="text" id="title" bind:value={title} />
+    </div>
+    <div class="form-control">
+      <label for="subtitle">Subtitle</label>
+      <input type="text" id="subtitle" bind:value={subtitle} />
+    </div>
+    <div class="form-control">
+      <label for="address">Adress</label>
+      <input type="text" id="address" bind:value={address} />
+    </div>
+    <div class="form-control">
+      <label for="imageUrl">Image URL</label>
+      <input type="text" id="imageUrl" bind:value={imageUrl} />
+    </div>
+    <div class="form-control">
+      <label for="email">Email</label>
+      <input type="email" id="email" bind:value={email} />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <textarea rows="3" id="description" bind:value={description} />
+    </div>
+    <button type="submit">Save</button>
+  </form>
+  <MeetupGrid {meetups} />
+</main>
 
 <style>
-  #meetups {
+  main {
     margin-top: 5rem;
   }
 </style>
