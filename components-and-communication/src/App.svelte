e10c3bfa60a4716beb05e7ae8b24d2312dfccdf1
Matthew Doles <matteddied@gmail.com>
2021-10-20 15:35:52 -0400

    6.2 - Spread Props, Default Props, & Slots

--- components-and-communication/src/App.svelte
+++ components-and-communication/src/App.svelte
@@ -1,9 +1,29 @@
 <script>
+  import Modal from './Modal.svelte';
   import Product from './Product.svelte';
+
+  let products = [
+    {
+      id: 'p1',
+      title: 'A book',
+      price: 9.99,
+    },
+  ];
+
+  function addToCart(event) {
+    console.log(event);
+  }
+
+  function deleteProduct(event) {
+    console.log(event.detail);
+  }
 </script>
 
-<Product
-  productTitle="A Book"
-  on:add-to-cart={() => alert('Add to cart!')}
-  on:delete={() => alert('Deleted!')}
-/>
+{#each products as product}
+  <Product {...product} on:add-to-cart={addToCart} on:delete={deleteProduct} />
+{/each}
+
+<Modal>
+  <h1>Hello</h1>
+  <p>This works...</p>
+</Modal>
