76565d9203940182deb2547ced2ffaf864f499e1
Matthew Doles <mdoles@its.jnj.com>
2021-11-22 17:46:53 -0500

    8.1 - Custom Component Bindings

---
+++ bindings-and-forms/src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import CustomInput from './CustomInput.svelte';
+  import Toggle from './Toggle.svelte';
+
+  let val = 'Matthew';
+  let selectedOption = 2;
+
+  function setValue(event) {
+    val = event.target.value;
+  }
+</script>
+
+<!-- <input type="text" value={val} on:input={setValue} /> -->
+<!-- <input type="text" bind:value={val} /> -->
+<CustomInput bind:val />
+<Toggle choosenOption={selectedOption} />
---
+++ bindings-and-forms/src/Toggle.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let choosenOption = 1;
+</script>
+
+<button disabled={choosenOption === 1} on:click={() => (choosenOption = 1)}
+  >Option 1</button
+>
+<button disabled={choosenOption === 2} on:click={() => (choosenOption = 2)}
+  >Option 2</button
+>
