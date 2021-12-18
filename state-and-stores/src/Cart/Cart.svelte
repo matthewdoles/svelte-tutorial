ccf9d1099e439df28ffab6b20f9a4e02088d189b
Matthew Doles <matteddied@gmail.com>
2021-12-18 10:42:27 -0500

    10.3 - Managing Store Subscriptions & Autosubscriptions

--- state-and-stores/src/Cart/Cart.svelte
+++ state-and-stores/src/Cart/Cart.svelte
@@ -1,12 +1,19 @@
 <script>
+  import { onDestroy } from 'svelte';
   import cartItems from './cart-store';
   import CartItem from './CartItem.svelte';
 
-  let items;
+  // let items;
 
-  cartItems.subscribe((its) => {
-    items = its;
-  });
+  // const unsubscribe = cartItems.subscribe((its) => {
+  //   items = its;
+  // });
+
+  // onDestroy(() => {
+  //   if (unsubscribe) {
+  //     unsubscribe();
+  //   }
+  // });
 
   // export let items = [
   //   {
@@ -25,7 +32,7 @@
 <section>
   <h1>Cart</h1>
   <ul>
-    {#each items as item (item.id)}
+    {#each $cartItems as item (item.id)}
       <CartItem id={item.id} title={item.title} price={item.price} />
     {:else}
       <p>No items in cart yet!</p>
