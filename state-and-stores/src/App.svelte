82ba3426e6fe78e7d748d8c2c7cb3f65b5c4defb
Matthew Doles <matteddied@gmail.com>
2021-12-20 19:10:25 -0500

    10.5 - Readable Stores & Custom Stores

--- state-and-stores/src/App.svelte
+++ state-and-stores/src/App.svelte
@@ -2,8 +2,13 @@
   import Cart from './Cart/Cart.svelte';
   import Products from './Products/Products.svelte';
   import Button from './UI/Button.svelte';
+  import { timer } from './timer-store';
 
   let showCart = true;
+
+  // timer.subscribe((count) => {
+  //   console.log('App: ' + count);
+  // });
 </script>
 
 <Button
@@ -15,3 +20,5 @@
   <Cart />
 {/if}
 <Products />
+
+<!-- <p>Count: {$timer}</p> -->
--- state-and-stores/src/Cart/Cart.svelte
+++ state-and-stores/src/Cart/Cart.svelte
@@ -2,6 +2,11 @@
   import { onDestroy } from 'svelte';
   import cartItems from './cart-store';
   import CartItem from './CartItem.svelte';
+  import { timer } from '../timer-store';
+
+  const unsubscribe = timer.subscribe((count) => {
+    console.log('Cart: ' + count);
+  });
 
   // let items;
 
@@ -9,11 +14,11 @@
   //   items = its;
   // });
 
-  // onDestroy(() => {
-  //   if (unsubscribe) {
-  //     unsubscribe();
-  //   }
-  // });
+  onDestroy(() => {
+    if (unsubscribe) {
+      unsubscribe();
+    }
+  });
 
   // export let items = [
   //   {
