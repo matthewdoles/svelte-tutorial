9cc5a10e747b14c8a839a1a76429ebb1e83aaad3
Matthew Doles <matteddied@gmail.com>
2022-01-06 13:16:29 -0500

    12.3 - Element Transitions

--- motion-transitions-animations/src/App.svelte
+++ motion-transitions-animations/src/App.svelte
@@ -3,6 +3,7 @@
   import { tweened } from 'svelte/motion';
   import { cubicIn } from 'svelte/easing';
   import Spring from './Spring.svelte';
+  import { fade, fly, slide, scale } from 'svelte/transition';
 
   const progress = tweened(0, {
     delay: 0,
@@ -13,7 +14,45 @@
   setTimeout(() => {
     progress.set(0.5);
   }, 1500);
+
+  let boxes = [];
+  let boxInput;
+
+  function addBox() {
+    boxes = [...boxes, boxInput.value];
+  }
+  function discard(value) {
+    boxes = boxes.filter((el) => el !== value);
+  }
 </script>
 
 <!-- <progress value={$progress} /> -->
-<Spring />
+<!-- <Spring /> -->
+
+<input type="text" bind:this={boxInput} />
+<button on:click={addBox}>Add</button>
+{#each boxes as box (box)}
+  <div
+    transition:fly={{
+      delay: 0,
+      duration: 400,
+      opacity: 0,
+      x: 300,
+    }}
+    on:click={discard.bind(this, box)}
+  >
+    {box}
+  </div>
+{/each}
+
+<style>
+  div {
+    width: 10rem;
+    height: 10rem;
+    background: #ccc;
+    margin: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 5px;
+    padding: 1rem;
+  }
+</style>
