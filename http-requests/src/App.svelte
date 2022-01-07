278e8136a65c818214cfffd088d50ae1fc0d3857
Matthew Doles <matteddied@gmail.com>
2022-01-06 19:09:56 -0500

    14.1 - Sending Data via a POST Request

---
+++ http-requests/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  let hobbies = [];
+  let hobbyInput;
+
+  function addHobby() {
+    hobbies = [...hobbies, hobbyInput.value];
+
+    fetch(
+      'https://svelte-course-doles-default-rtdb.firebaseio.com/hobbies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(hobbies),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Failed!');
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+</script>
+
+<label for="hobby">Hobby</label>
+<input type="text" id="hobby" bind:this={hobbyInput} />
+<button on:click={addHobby}>Add Hobby</button>
+
+<ul>
+  {#each hobbies as hobby}
+    <li>{hobby}</li>
+  {/each}
+</ul>
