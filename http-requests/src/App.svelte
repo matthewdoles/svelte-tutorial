c0a3932db519f080192ff5aa55c0175154944325
Matthew Doles <matteddied@gmail.com>
2022-01-07 09:13:51 -0500

    14.2 - Fetching & Transforming Data

--- http-requests/src/App.svelte
+++ http-requests/src/App.svelte
@@ -1,15 +1,44 @@
 <script>
+  import { onMount } from 'svelte';
+
   let hobbies = [];
   let hobbyInput;
+  let isLoading = true;
+
+  onMount(() => {
+    fetch(
+      'https://svelte-course-doles-default-rtdb.firebaseio.com/hobbies.json'
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        hobbies = Object.values(data);
+        let keys = Object.keys(data);
+        console.log(keys);
+
+        for (const key in data) {
+          console.log(key, data[key]);
+        }
+      })
+      .catch((err) => console.log(err))
+      .finally(() => {
+        isLoading = false;
+      });
+  });
 
   function addHobby() {
     hobbies = [...hobbies, hobbyInput.value];
 
+    isLoading = true;
     fetch(
       'https://svelte-course-doles-default-rtdb.firebaseio.com/hobbies.json',
       {
         method: 'POST',
-        body: JSON.stringify(hobbies),
+        body: JSON.stringify(hobbyInput.value),
         headers: { 'Content-Type': 'application/json' },
       }
     )
@@ -18,7 +47,10 @@
           throw new Error('Failed!');
         }
       })
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(() => {
+        isLoading = false;
+      });
   }
 </script>
 
@@ -26,8 +58,12 @@
 <input type="text" id="hobby" bind:this={hobbyInput} />
 <button on:click={addHobby}>Add Hobby</button>
 
-<ul>
-  {#each hobbies as hobby}
-    <li>{hobby}</li>
-  {/each}
-</ul>
+{#if isLoading}
+  <p>Loading...</p>
+{:else}
+  <ul>
+    {#each hobbies as hobby}
+      <li>{hobby}</li>
+    {/each}
+  </ul>
+{/if}
